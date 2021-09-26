f411057dd24b05bcd48e7e7a039132a6f6f685e0
hernan <Hernantkup@gmail.com>
2021-09-26 19:07:17 -0300

    ok

--- app/screens/country-statistics/StatsItem.js
+++ app/screens/country-statistics/StatsItem.js
@@ -5,7 +5,23 @@ const StatsItem = ({item}) => {
     return (
             <Card>
                 <Heading size="md">{item.country}</Heading>
+                <Text>Populacao: {item.population}</Text>
+            <Card>
+                    <Heading>Casos</Heading>
+                    <Text>Ativos: {item.cases.active}</Text>
+                    <Text>Novos: {item.cases.new}</Text>
+                    <Text>Total: {item.cases.total}</Text>
+                </Card>
+                <Card>
+                    <Heading>Mortes</Heading>
+                    <Text>Novos: {item.death.new}</Text>
+                    <Text>Total: {item.death.total}</Text>
+                </Card> 
 
+                <Card>
+                    <Heading>Testes</Heading>
+                    <Text>Total: {item.tests.total}</Text>
+                </Card>
  
             </Card>
     )
--- app/screens/history/HistoryItem.js
+++ app/screens/history/HistoryItem.js
@@ -5,9 +5,26 @@ const HistoryItem = ({item}) => {
     return (
             <Card>
                 <Heading size="md">{item.country} Dia: {item.day}</Heading>
+                <Text>Populacao: {item.population}</Text>
+                <Card>
+                    <Heading>Casos</Heading>
+                    <Text>Ativos: {item.cases.active}</Text>
+                    <Text>Novos: {item.cases.new}</Text>
+                    <Text>Total: {item.cases.total}</Text>
+                </Card>
+                <Card>
+                    <Heading>Mortes</Heading>
+                    <Text>Novos: {item.death.new}</Text>
+                    <Text>Total: {item.death.total}</Text>
+                </Card> 
 
+                <Card>
+                    <Heading>Testes</Heading>
+                    <Text>Total: {item.tests.total}</Text>
+                </Card>
  
             </Card>
+ 
     )
 }
 
